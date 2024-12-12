6f3a38a8eb7cf53bab4f84101c23915543214fe5
JolitaM <jolita.molyte@chgf.stud.vu.lt>
2024-12-12 02:02:14 +0200

    pakeitimas su stiliais

--- 3praktinis/js/map.js
+++ 3praktinis/js/map.js
@@ -1,9 +1,9 @@
 // Kodas inicijuojantis žemėlapio objektą div elemente map
 var map = new maplibregl.Map({
   container: "map",
-  style: "styles/default_style.json",
+  style: "styles/overview.json",
   hash: true,
-  center: [24.007, 55.073],
+  center: [24.000, 55.073],
   zoom: 6,
 });
 
@@ -15,12 +15,12 @@ function switchBgLayers(layerName) {
   var layer;
 
   // Pagal funkcijos parametrą parenkamas žemėlapio stilius
-  if (layerName === "Gatvės") {
+  if (layerName === "Topografinis") {
     layer = "https://basemap.startupgov.lt/vector/styles/bright/style.json";
-  } else if (layerName === "Pilkas") {
+  } else if (layerName === "Apžvalginis") {
     layer = "https://basemap.startupgov.lt/vector/styles/positron/style.json";
   } else {
-    layer = "styles/default_style.json";
+    layer = "styles/overview.json";
   }
 
   // maplibre setStyle funkcija pakeičia žemėlapio stilių
